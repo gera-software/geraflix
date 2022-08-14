b1c7e66fbc3d7ae9f48d35797b160a5a1ad1c25e
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-08-14 13:00:15 -0300

    bot receive start command

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello World!')
+const { Telegraf, Markup, session } = require('telegraf')
+require('dotenv').config()
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+const bot = new Telegraf(token)
+
+bot.start(async ctx => {
+    console.log('START')
+
+    const messageFrom = ctx.update.message.from
+    const messageChat = ctx.update.message.chat
+
+    console.log(messageFrom)
+    console.log(messageChat)
+})
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
