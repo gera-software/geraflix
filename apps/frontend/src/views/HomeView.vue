874f99caa8529c695beff1dad384cac0467bf339
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-10-19 21:29:56 -0300

    feat: home view basic style

--- apps/frontend/src/views/HomeView.vue
+++ apps/frontend/src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <button >new room TODO</button>
+    <div class="container-center">
+        <h1>Geraflix</h1>
+        <h2>Compartilhe momentos cinematográficos à distância!</h2>
+        <button class="btn-primary">Criar sala</button>
     </div>
 </template>
 
@@ -9,4 +11,32 @@
 </script>
 
 <style scoped>
+
+.container-center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    padding: 20px;
+    text-align: center;
+}
+
+.btn-primary {
+    background-color: #F5811B;
+    border: none;
+    padding: 20px;
+    font-size: 1.2em;
+    border-radius: 8px;
+    color: white;
+    cursor: pointer;
+}
+
+.btn-primary:hover {
+    background-color: #f18f3a;
+}
+
+.btn-primary:active {
+    background-color: #cc6c18;
+}
 </style>
